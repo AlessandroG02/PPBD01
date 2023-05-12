01f7f221a9415d31b8e2a8e767be95906a0f88e4
Chay-06 <130737504+Chay-06@users.noreply.github.com>
2023-05-12 17:33:32 +0000

    Add files via upload

---
+++ _personale/ese_01.py
@@ -0,0 +1,10 @@
+def fahrenheit_to_celsius(f):
+    temperatura = (f - 32) *5 /9
+    return temperatura
+
+#l'esercizio chiede di non stampare, ma lo faccio per
+# verificare il funzionamento dello script
+print(fahrenheit_to_celsius(90))
+
+temp1 = fahrenheit_to_celsius(37)
+print(temp1)
---
+++ _personale/ese_01b.py
@@ -0,0 +1,10 @@
+def mi_to_km(mi):
+    km = mi * 1.609
+    return km
+
+#l'esercizio chiede di non stampare, ma lo faccio per
+# verificare il funzionamento dello script
+print(mi_to_km(60))
+
+distanza = mi_to_km(30)
+print(distanza)
