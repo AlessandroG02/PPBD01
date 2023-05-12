74ecc7d9388cebecdb08ee9742c3fbc298e70d7b
Chay-06 <130737504+Chay-06@users.noreply.github.com>
2023-05-12 18:30:36 +0000

    Add files via upload

--- _personale/ese_03.py
+++ _personale/ese_03.py
@@ -8,48 +8,21 @@ Senza contare le rivisitazioni più raffinate come la crostata morbida o la tort
 
 parola_malefica1 = "Tiramisù"
 parola_malefica2 = "nutella"
-parola_1_var1 = parola_malefica1.upper()
-parola_1_var2 = parola_malefica1.lower()
-parola_2_var1 = parola_malefica2.upper()
-parola_2_var2 = parola_malefica2.lower()
- 
-parola_malefica01 = [parola_malefica1, parola_1_var1, parola_1_var2]
-parola_malefica02 = [parola_malefica2, parola_2_var1, parola_2_var2]
 censura = "******"
-testo_censurato = ''
 
-testo2 = testo.split(" ")
-for parola in testo2:
-    if parola in parola_malefica01 or parola_malefica02:
-        testo_censurato = testo2.replace(parola, censura)
+def change_case(convertibile):
+    convertito = convertibile.lower()
+    return convertito
 
-
-print(testo_censurato)
-
-
-# secondo tentativo:
-parole_proibite = []
-
-def all_cases(maiu, minu):
-    a = maiu.upper()
-    b = minu.lower()
-    return a, b
-
-all_cases(parola_malefica1)
-parola_malefica1.append(a)
+testo2 = change_case(testo)
+nuova_parola1 = change_case(parola_malefica1)
+nuova_parola2 = change_case(parola_malefica2)
 
 
+testo3 = testo2.replace(nuova_parola1, censura)
+testo3 = testo3.replace(nuova_parola2, censura)
 
-#terzo tentativo:
-
-parola1 = parola_malefica1.casefold
-parola2 = parola_malefica2.casefold
-testo = testo.split(' ')
-for parola in testo:
-    if parola1 or parola2 in testo:
-        testo_censurato = testo.replace(parola, censura)
-
-print(testo_censurato)
+conteggio = testo3.count(censura)
 
 
-# usando lambda
+print(testo3, '\n', 'Censurate', conteggio, 'parole.')
