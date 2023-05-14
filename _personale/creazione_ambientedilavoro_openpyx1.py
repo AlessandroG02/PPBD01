a882001c6bd92f9aedcb51542cd0628278fc301f
Andrea Corraro <andrea.corraro0190@gmail.com>
2023-05-14 17:26:44 +0200

    Update: codice corretto

--- _personale/creazione_ambientedilavoro_openpyx1.py
+++ _personale/creazione_ambientedilavoro_openpyx1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from openpyxl import Workbook
 
 nuovo_book = Workbook()          # creo l'oggetto per interfacciarmi col workbook
@@ -14,24 +13,3 @@ nuovo_sheet['A4'] = 'criceto'
 
 # salvo il file (come fare un "flush")
 nuovo_book.save('./_personale/nuovo_file.xlsx')
-
-
-=======
-from openpyxl import Workbook
-
-nuovo_book = Workbook()          # creo l'oggetto per interfacciarmi col workbook
-nuovo_sheet = nuovo_book.active  # seleziono il worksheet attivo
-
-nuovo_sheet.title = "creazione_file_xlsx"
-
-# scrivo i dati uno alla volta
-nuovo_sheet['A1'] = 'gatto'
-nuovo_sheet['A2'] = 'cane'
-nuovo_sheet['A3'] = 'pappagallo'
-nuovo_sheet['A4'] = 'criceto'
-
-# salvo il file (come fare un "flush")
-nuovo_book.save('./files_esercizi/outputs/nuovo_file.xlsx')
-
-
->>>>>>> c25a28e428a2623ad21f21541ae17808837b359d
