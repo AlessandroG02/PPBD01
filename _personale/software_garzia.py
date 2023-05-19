656c8f015da3b8886153eeaa0390e75238ce99aa
andrea corraro <andrea.corraro0190@gmail.com>
2023-05-19 16:05:14 +0200

    Update: continuo e modifica software Garzia

--- _personale/software_garzia.py
+++ _personale/software_garzia.py
@@ -1,18 +1,28 @@
 import pandas as pd
-from openpyxl import workbook
+from openpyxl import Workbook
+from openpyxl.utils import dataframe
 
-df = pd.read_excel('DB C-Lab (HRR).xlsx',           # leggi il file excel
-sheet_name='AnagSkill')
+df_anag = pd.read_excel('DB C-Lab (HRR).xlsx',           # leggi il file excel
+     sheet_name='AnagSkill')                         #legge il foglio
 
-df_righe = df.iloc[[0, 1, 2]]      # estrai le righe dal data frame
+df_righe = df_anag.iloc[[0, 1, 2]]      # estrai le righe dal data frame
 
-new_book = workbook()           # crea un nuovo file excel
+df_candidati = pd.read_excel('DB C-Lab (HRR).xlsx',           # leggi il file excel
+     sheet_name='Candidati')                         #legge il foglio
+
+# df_righe_2 = df_candidati.iloc[[0, 1, 2]] 
+print(df_candidati)
+
+new_book = Workbook()           # crea un nuovo file excel
+
 new_sheet = new_book.active         # seleziona il foglio di lavoro attivo
+new_sheet.title = "AnagSkill"
 
-new_sheet.title = "crezione_file_xlsx"
+new_book.create_sheet(title='Candidati')
+
 
 # scrivi i dati del dataframe nel foglio di lavoro
-for r in dataframe_to_rows(df_righe, index=False, header=True):
+for r in dataframe.dataframe_to_rows(df_righe, index=False, header=True):
     new_sheet.append(r)
 
 # salva il file excel
