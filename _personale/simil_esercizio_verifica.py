664428904d84b3c9b30e1c99b111d45786748765
andrea.corraro <andrea.corraro0190@gmail.com>
2023-05-23 17:02:53 +0100

    Update: modifica codice

--- _personale/simil_esercizio_verifica.py
+++ _personale/simil_esercizio_verifica.py
@@ -1,17 +1,24 @@
 import csv
 
+identifier = 'ID'                                       # identificatore dei record
+col_names = ['ID', 'Ragione sociale', 'Comune', 'Cap']  # colonne da estrarre
+record_ids = [45, 64, 176, 204]                         # record da estrarre
+sep_rec = '----------------------------'                # separatore visivo da usare
+
 tot_comuni = 0
 tot_abitanti = 0
 
-with open('../../files_esercizi/Sardegna_centri_urbani_per_abitante_e_altitudine_2014-01-13.csv',
+with open('./files_esercizi/Sardegna_centri_urbani_per_abitante_e_altitudine_2014-01-13.csv',
           encoding='latin-1') as file_in:
-    reader_obj = csv.reader(file_in, delimiter=';')  # ottengo una lista di liste (come iterabile)
+    reader_obj = csv.DictReader(file_in, delimiter=';')  # ottengo una lista di liste (come iterabile)
     next(reader_obj)             # salto la prima riga, dove c'è l'intestazione
-    for linea in reader_obj:     # per ciascuna lista (la riga, che corrisponde a un record/comune)
-        if int(linea[5]) > 600:  # se il valore nella sesta colonna (index 5) è maggiore di 600
-            tot_comuni += 1      # incrementa il contatore dei comuni di 1
-            tot_abitanti += int(linea[4])  # incrementa il contatore abitanti del numero di
-                                           #    abitanti del comune corrente (colonna 5, index 6)
+    for linea in file_reader:
+        if linea[identifier] in [str(idn) for idn in record_ids]:  # filtro
+            for col_name in col_names:
+                print(col_name+':', linea[col_name])
+            print(sep_rec)
+        else:
+            pass
 
 print('N. centri urbani sopra i 600 m s.l.m.:', tot_comuni)
 print('N. abitanti sopra i 600 m s.l.m.:', tot_abitanti)
