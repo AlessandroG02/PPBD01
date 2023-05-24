1aa8cd83f79c0e7a02213493436d7fed7aecb772
andrea corraro <andrea.corraro0190@gmail.com>
2023-05-24 11:11:49 +0200

    Update: modifica verifica

--- _personale/Verifica_di_apprendimento_PROVA_PRATICA.py
+++ _personale/Verifica_di_apprendimento_PROVA_PRATICA.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import csv
 from pprint import pprint
 
-file_path = 'Comune-di-Torino---Attivita-commerciali.csv'
+file_path = (.\personale\'Comune-di-Torino---Attivita-commerciali.csv')
 
 
 
@@ -24,21 +23,12 @@ def conta_attività(pathFile):
     print(f'Numero attività commerciali trovate: {counter}')
 
 conta_attività(file_path)
-=======
-# scrivi qui
-import json
-from pprint import pprint
-counter=0
-lista_cap=["10126", "10127","10134"]
-file_path='C:/Users/Utente/OneDrive/Documents/PPBD01/_personale/Comune-di-Torino---Attivita-commerciali.json'
-surf=150
 
-with open(file_path,
-          encoding='latin-1') as file_in:
-            reader_obj = json.load(file_in)
-            for attivita in reader_obj:
-                        if attivita['Mq tot locale'].isdigit():
-                          if int(attivita['Anno inizio attivita'])>=2011 and int(attivita['Mq tot locale'])>=surf and attivita['Cap'] in lista_cap:
-                                counter+=1
-pprint(f"Numero attività commerciali trovate: {counter}")   
->>>>>>> 24811b8eddcc5a228454093b798119dc93913015
+
+import csv
+from pprint import pprint
+
+file_path = 'Comune-di-Torino---Attivita-commerciali.csv'
+
+
+
